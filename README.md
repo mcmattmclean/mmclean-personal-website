77a8c64fbad87e31c934b2d3a60e969eb362db36
Matthew McLean <matthewmclean21@gmail.com>
2017-11-21 10:59:59 -0800

    Prep for migration to nodejs

--- README.md
+++
@@ -1,4 +0,0 @@
-# README
-
-This is my personal website I'll be using to explore what I can do with
-Ruby on Rails, SCSS, etc.
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-   root to: 'pages#home'
-   get 'about', to: 'pages#about'
-   get 'resume', to: 'pages#resume'
-   get 'contact', to: 'pages#contact'
-end
